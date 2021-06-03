aa67401c4211ffc10689b5627f83821bc4fb2817
Deepak <deeebeee3@hotmail.com>
2021-06-03 18:38:03 +0100

    update react dnd packages to latest and create useDragItem hook

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { App } from "./App";
 import { AppStateProvider } from "./state/AppStateContext";
 import { DndProvider } from "react-dnd";
-import HTML5Backend from "react-dnd-html5-backend";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 ReactDOM.render(
   <React.StrictMode>
